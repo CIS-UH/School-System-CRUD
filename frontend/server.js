33a727d016fe8a6651e22b07eae486c5a6b91b46
michael <mvmnr01@gmail.com>
2024-04-22 10:11:34 -0500

    problem: login successful even if pw wrong

--- frontend/server.js
+++ frontend/server.js
@@ -4,21 +4,27 @@ const path = require('path');
 
 //put new express application on app variable
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 //axios
 const axios = require('axios');
+const config = {
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  }
+};
 
 //set views property and views engine
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
-flaskApi = 'https://localhost:5000/your-endpoint';
-
 const port = 8081;
 
 var auth = false;
 
-//when user hits home page, then we show the hello.ejs page
+//force user to login if not authorized
 app.get('/', (req, res) => {
   if(!auth){
     res.redirect('/login');
@@ -35,20 +41,33 @@ app.get('/login', (req, res) => {
 
 });
 
-app.post('/login', (req,res)=>{
-  const userData = req.body;
+app.post('/login', async(req,res)=>{
 
+  // get login info from ejs form
+  const userLoginInfo = req.body;
+
+  console.log(userLoginInfo);
+
+  // attempt login
   try {
-    const response = axios.post("https:localhost:5000/api/login", userData)
-    console.log('login successful')
-    auth = true
-    res.redirect('/')
+    const response = await axios.post("http://localhost:5000/api/login", userLoginInfo);
+    
+    console.log(response.data)
+
+    //redirect to home page if login attempt is successful
+    if(response.data.message == 'Login successful'){
+      auth = true;
+      res.redirect('/');
+    }
+
+
   }
+  // catch error if login failed
   catch(error){
-    console.error("login failed:", error);
+    console.log("login failed");
     
   }
-})
+});
 
 
 //start the express application on port 8081 and print server start message
