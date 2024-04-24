62dc7242e72abee6417f0e1b1902c8e78c8e31ed
michael <mvmnr01@gmail.com>
2024-04-24 12:43:52 -0500

    teacher GET not wotking

--- frontend/server.js
+++ frontend/server.js
@@ -24,12 +24,48 @@ const port = 8081;
 
 var auth = false;
 
+app.get('/facility', (req, res) => {
+  res.render('facility', {getData: null}); 
+});
+
+app.post('/facility', async(req,res) =>{
+  var getData = {}
+  try {
+    const response = await axios.get('http://localhost:5000/api/facility');
+    console.log(response.data);
+    res.render('facility', {getData: JSON.stringify(response.data)});
+  }
+  catch(error){
+    console.log(error);
+  }
+});
+
 app.get('/teacher', (req, res) => {
-  res.render('teacher'); // Assuming you have a teacher.ejs file in your views folder
+  res.render('teacher', {getData: null}); 
+});
+
+app.post('/teacher', async (req, res) => {
+  var room_id = parseInt(req.body.room_id);
+  console.log(room_id);
+  try {
+      // Make API call
+      const apiResponse = await axios.get('http://localhost:5000/api/teacher', {room: room_id});
+
+      // Extract data from API response
+      const data = apiResponse.data;
+      console.log(data);
+
+      // Render EJS template with data
+      res.render('teacher', { getData: JSON.stringify(data) });
+  } catch (error) {
+      // Handle errors
+      console.error('Error fetching data:', error);
+      res.status(500).send('Error fetching data', error);
+  }
 });
 
 app.get('/child', (req, res) => {
-  res.render('child'); // Assuming you have a teacher.ejs file in your views folder
+  res.render('child'); 
 });
 
 //force user to login if not authorized
@@ -78,22 +114,7 @@ app.post('/login', async(req,res)=>{
   }
 });
 
-app.post('/teacher/get', async (req, res) => {
-  try {
-      // Make API call
-      const apiResponse = await axios.get('http://localhost:5000/api/teacher/all');
 
-      // Extract data from API response
-      const data = apiResponse.data;
-      console.log(data);
-      // Render EJS template with data
-      res.render('teacher', { data });
-  } catch (error) {
-      // Handle errors
-      console.error('Error fetching data:', error);
-      res.status(500).send('Error fetching data', error);
-  }
-});
 
 //start the express application on port 8081 and print server start message
 app.listen(port, () => console.log('Application started on localhost:8081'));
